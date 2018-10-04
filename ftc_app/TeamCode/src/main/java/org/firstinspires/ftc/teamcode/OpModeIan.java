76a2a0a4d1bc1124367c71cd5fbfe3f281086f45
Ian Cowley <ian@tidelaundry.com>
2018-10-04 15:01:07 -0500

    Initial commit of open cv library.

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeIan.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeIan.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name = "Op Mode Ian", group = "Ian")
 public class OpModeIan extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
+    private GoldAlignDetector goldAlignDetector = new GoldAlignDetector();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,12 +25,33 @@ public class OpModeIan extends LinearOpMode {
 
         DriveUtils.getInstance().init(leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor);
 
+        initGoldAlignDetector();
+
         waitForStart();
         runtime.reset();
 
         while(opModeIsActive()) {
-            DriveUtils.getInstance().driveForward(100);
-            DriveUtils.getInstance().turnRight(90);
+            telemetry.addData("goldAlignDetector.isAligned", goldAlignDetector.getAligned());
+            telemetry.addData("goldAlignDetector.xPosition", goldAlignDetector.getXPosition());
         }
+    }
+
+    private void initGoldAlignDetector() {
+        goldAlignDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        goldAlignDetector.useDefaults();
+
+        // Optional Tuning
+        goldAlignDetector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        goldAlignDetector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        goldAlignDetector.downscale = 0.4; // How much to downscale the input frames
+
+        goldAlignDetector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        goldAlignDetector.maxAreaScorer.weight = 0.005;
+
+        goldAlignDetector.ratioScorer.weight = 5;
+        goldAlignDetector.ratioScorer.perfectRatio = 1.0;
+
+        goldAlignDetector.enable();
     }
 }
