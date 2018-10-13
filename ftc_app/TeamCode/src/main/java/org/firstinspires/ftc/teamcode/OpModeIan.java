66643c57f59ac70896eb5c86be0f2f3175d47f0d
Ian Cowley <ian@tidelaundry.com>
2018-10-13 12:05:58 -0500

    Fixed DogeCV on my laptop.

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeIan.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeIan.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -7,6 +10,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class OpModeIan extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
+    private GoldAlignDetector detector;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -20,10 +24,34 @@ public class OpModeIan extends LinearOpMode {
 
         waitForStart();
         runtime.reset();
+        initGoldAlignDetector();
 
         while(opModeIsActive()) {
-            DriveUtils.getInstance().driveForward(100);
-            DriveUtils.getInstance().turnRight(90);
+            telemetry.addData("IsAligned" , detector.getAligned()); // Is the bot aligned with the gold mineral
+            telemetry.addData("X Pos" , detector.getXPosition()); // Gold X pos.
         }
+
+        detector.disable();
+    }
+
+    private void initGoldAlignDetector() {
+        detector = new GoldAlignDetector();
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        detector.useDefaults();
+
+        // Optional Tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005;
+
+        detector.ratioScorer.weight = 5;
+        detector.ratioScorer.perfectRatio = 1.0;
+
+        detector.enable();
+
     }
 }
